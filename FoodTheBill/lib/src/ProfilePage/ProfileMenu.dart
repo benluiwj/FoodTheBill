3b387b51d58b6d6a8af95e489007f822f5ee9091
benluiwj <wenjie.lui@gmail.com>
2021-07-22 16:48:39 +0800

    added profile screen

---
+++ FoodTheBill/lib/src/ProfilePage/ProfileMenu.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+class ProfileMenu extends StatelessWidget {
+  const ProfileMenu(
+      {Key? key, required this.text, required this.icon, required this.press})
+      : super(key: key);
+  final String text, icon;
+  final Function()? press;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+      child: TextButton(
+        style: TextButton.styleFrom(
+          padding: EdgeInsets.all(20),
+          shape:
+              RoundedRectangleBorder(borderRadius: BorderRadius.circular(15)),
+          backgroundColor: Colors.grey[300],
+        ),
+        onPressed: press,
+        child: Row(children: [
+          SvgPicture.asset(
+            icon,
+            width: 22,
+            color: Colors.black,
+          ),
+          SizedBox(width: 20),
+          Expanded(
+              child: Text(
+            text,
+            style: Theme.of(context).textTheme.bodyText1,
+          )),
+          Icon(
+            Icons.arrow_forward_ios,
+            color: Colors.black,
+          )
+        ]),
+      ),
+    );
+  }
+}
--- FoodTheBill/lib/src/ProfilePage/ProfileScreenBody.dart
+++ FoodTheBill/lib/src/ProfilePage/ProfileScreenBody.dart
@@ -1,13 +1,21 @@
 import 'package:flutter/material.dart';
 import 'package:milestone1/src/ProfilePage/ProfilePic.dart';
+import 'ProfileMenu.dart';
 
 class ProfileScreenBody extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return SingleChildScrollView(
         padding: EdgeInsets.symmetric(vertical: 20),
-        child: Column(
-          children: [ProfilePic()],
-        ));
+        child: Column(children: [
+          ProfilePic(),
+          SizedBox(height: 20),
+          ProfileMenu(
+              text: "Favourites",
+              icon: "assets/Heart Icon_2.svg",
+              press: () => {}),
+          ProfileMenu(
+              text: "Log out", icon: "assets/Log out.svg", press: () => {})
+        ]));
   }
 }
